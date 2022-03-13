d8a530c36db4d72b51db20f2449920914000c793
Junhao <2428393291@qq.com>
2022-03-13 21:13:03 +0800

    first

---
+++ src/main/java/com/example/testopenfeignserver/TestOpenFeignServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.testopenfeignserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestOpenFeignServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestOpenFeignServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/testopenfeignserver/TestOpenFeignServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testopenfeignserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestOpenFeignServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
